4e3a239c0c7def9d8b71ebc7f53ffc35233d4d6f
Yuvprime <yuvztikode@gmail.com>
2015-05-07 15:39:30 +0530

    added app bar aka toolbar

--- app/src/main/java/com/materialnavigationdrawer/MainActivity.java
+++ app/src/main/java/com/materialnavigationdrawer/MainActivity.java
@@ -1,18 +1,26 @@
 package com.materialnavigationdrawer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar app_bar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        app_bar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(app_bar);
+
+
+
     }
 
     @Override
